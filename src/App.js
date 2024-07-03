e178125e2792879b9782b73976c7788b100f82a8
damsot <denispiyack@gmail.com>
2024-07-03 03:27:27 +0200

    1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './main-page/main-page'
 
 function App() {
   return (
     <div>
+      <MainPage />
       sasdasdasd
     </div>
   );
---
+++ src/main-page/main-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPageHello from './main-page-hello/main-page-hello';
+
+const MainPage = () => {
+    return (
+        <header>
+            <MainPageHello />
+            <h1>This is the header for the General Page</h1>
+        </header>
+    );
+};
+
+export default MainPage;
